920284b73a3422ea1ba05d48f8d57a85741075b8
nizzlo <nisalsamaranayake@gmail.com>
2019-07-22 14:42:20 +0530

    3 files modified, 4 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { FooterComponent } from './footer/footer.component';
 
 import { RouterModule, Routes } from '@angular/router';//Imported for routing
 
-import { TestComponent } from './test/test.component';
+
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing/routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { FeedBackComponent } from './feed-back/feed-back.component';
 
 
 /*const appRoutes: Routes = [
@@ -24,8 +26,8 @@ import { RoutingModule } from './routing/routing.module';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , RoutingModule],
-  declarations: [ AppComponent, HeaderComponent, FooterComponent, TestComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule , RoutingModule], //Here the routing module is imported.
+  declarations: [ AppComponent, HeaderComponent, FooterComponent, HomeComponent, ProfileComponent, FeedBackComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TestComponent } from '../test/test.component';
-import { HomeComponent } from '../home/home.component'; 
+import { HomeComponent } from '../home/home.component';
+import { ProfileComponent} from '../profile/profile.component'
+import { FeedBackComponent} from '../feed-back/feed-back.component'
 
 import { RouterModule, Routes } from '@angular/router';//Imported for routing
 
@@ -10,8 +11,9 @@ import { RouterModule, Routes } from '@angular/router';//Imported for routing
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'test',   component: TestComponent },
   { path: 'home',   component: HomeComponent },
+  { path: 'profile',   component: ProfileComponent },
+  { path: 'feedback',   component: FeedBackComponent }
 ];
 
 
@@ -21,7 +23,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   exports: [
-    RouterModule
+    RouterModule //imports the module appRoutes to root
   ]
 })
 
